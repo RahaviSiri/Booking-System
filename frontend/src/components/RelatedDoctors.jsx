4c47488077bbf4df144906007703b98049bc3239
RahaviSiri <rahavi24siri@gmail.com>
2025-01-08 10:38:19 +0530

    Add logic at doctor appointment

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,19 +1,26 @@
-import React, { useContext } from 'react'
-import { useNavigate } from "react-router-dom"
+import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/AppContext';
+import { useNavigate } from 'react-router-dom';
 
-
-const TopDoctors = () => {
+const RelatedDoctors = ({docId,speciality}) => {
 
     const navigate = useNavigate();
-    const { doctors }  = useContext(AppContext);
+    const { doctors } = useContext(AppContext);
+    const [relDoc,setRelDoc] = useState([]);
+
+    useEffect(() => {
+        if(doctors.length > 0 && speciality){
+            const doctorsData = doctors.filter((docs) => docs.speciality === speciality && docs._id !== docId)
+            setRelDoc(doctorsData);
+        }
+    },[docId,speciality,doctors])
 
     return (
         <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
-            <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
+            <h1 className='text-3xl font-medium'>Related Doctors</h1>
             <p className='sm:w-1/2 text-center text-sm'>Simply browse through our extensive list of trusted doctors.</p>
             <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
-                {doctors.slice(0,10).map((item,index) => (
+                {relDoc.slice(0,5).map((item,index) => (
                     <div key={index} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' onClick={() => {navigate(`/doctor-appointment/${item._id}`),scrollTo(0,0)}}>
                         <img src={item.image} alt="" className='bg-blue-50'/>
                         <div className='p-4'>
@@ -26,10 +33,10 @@ const TopDoctors = () => {
                         </div>
                     </div>
                 ))}
-            </div>
+            </div> 
             <button onClick={() => {navigate('/doctors'); scrollTo(0,0)}} className='bg-blue-100 text-gray-600 px-12 py-3 mt-10 rounded-full'>More</button>
         </div>
     )
 }
 
-export default TopDoctors
+export default RelatedDoctors
