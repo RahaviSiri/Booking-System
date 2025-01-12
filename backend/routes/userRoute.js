3e7e3b57ec1f9bb44e1697c3a0da72569aca667e
RahaviSiri <rahavi24siri@gmail.com>
2025-01-12 17:38:48 +0530

    Add changes in my appointment page with backend

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { bookAppointment, getUserData, loginUser, registerUser, updateProfile } from "../controllers/userController.js";
+import { bookAppointment, getUserAppointments, getUserData, loginUser, registerUser, updateProfile } from "../controllers/userController.js";
 import authUser from "../middleware/authUser.js";
 import upload from "../middleware/multer.js";
 
@@ -10,6 +10,8 @@ userRouter.post('/login-user', loginUser);
 userRouter.get('/get-user',authUser,getUserData);
 userRouter.post('/update-user', upload.single('image'), authUser, updateProfile);
 userRouter.post('/book-appointment',authUser, bookAppointment);
+userRouter.get('/get-user-appointment',authUser, getUserAppointments);
+getUserAppointments
 
 
 export default userRouter;
